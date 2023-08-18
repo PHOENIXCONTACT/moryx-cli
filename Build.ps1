75fe3942ac95dffb2495d5fef18116b4ea2c1084
Christian Siewert <csiewert@phoenixcontact.com>
2023-08-18 10:39:05 +0200

    Add CI pipeline and related files
    Add `build.yaml` to be run as GitHub Action and files that are
    needed to execute it.

---
+++ Build.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [switch]$Build,
+    [switch]$Licensing,
+    [switch]$Pack
+)
+
+# Load Toolkit
+. ".build\BuildToolkit.ps1"
+
+# Set MSBuild to latest version
+$MsBuildVersion = "latest";
+
+# Initialize Toolkit
+Invoke-Initialize -Version (Get-Content "VERSION");
+
+if ($Build) {
+    Invoke-Build ".\Moryx.Cli.sln"
+}
+
+if ($Licensing) {
+    Invoke-Licensing
+}
+
+if ($Pack) {
+    Invoke-PackAll -Symbols
+}
+
+Write-Host "Success!"
